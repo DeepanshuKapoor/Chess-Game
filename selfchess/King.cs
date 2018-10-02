64b631300932509342a6baf78d9cc6e16d1cd117
DynamicCoder <kdeepanshu1996@gmail.com>
2018-10-02 11:27:37 +0530

    Update King.cs

--- selfchess/King.cs
+++ selfchess/King.cs
@@ -2,24 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace Chess_Game
-{
-    class King : Piece
-    {
-
-        public King(String colour) : base(colour)
-        {
+namespace Chess_Game{
+    class King : Piece{
+        public King(String colour) : base(colour){
             Name = "K";
         }
 
-        internal override bool validateMove(Move move, ChessBoard board)
-        {
-            if ( Math.Abs(move.to.Item1-move.from.Item1) <= 1  && Math.Abs(move.to.Item2 - move.from.Item2) <= 1  &&
-                ((Math.Abs(move.to.Item1 - move.from.Item1) + Math.Abs(move.to.Item2 - move.from.Item2) > 0)))
-            {
-                return board.getSquare(move.to).Piece == null || board.getSquare(move.to).Piece.Colour != Colour;
+        internal override bool validateMove(Move move, ChessBoard board){
+            if ( Math.Abs(move.to.Item1-move.from.Item1) <= 1  &&
+                Math.Abs(move.to.Item2 - move.from.Item2) <= 1  &&
+                ((Math.Abs(move.to.Item1 - move.from.Item1) + Math.Abs(move.to.Item2 - move.from.Item2) > 0))){
+                return board.getSquare(move.to).Piece == null ||
+                       board.getSquare(move.to).Piece.Colour != Colour;
             }
             return false;
         }
